a421a0f0272918ca3b49ac32e0b9791e8ba77d3f
mahdi <mahdi.taleghani@hotmail.com>
2022-11-08 18:17:21 +0330

    add Google Analytics Tag Code

---
+++ resources/views/layouts/GA.blade.php
@@ -0,0 +1,12 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-JXQH3N11C2"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+
+    gtag('js', new Date());
+    gtag('config', {{ env('GA_CODE') }});
+</script>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
+        @include('layouts.GA')
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
