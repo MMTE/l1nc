2936b01f0f5754e0e6b8a24334d7431b09542baa
mahdi <mahdi.taleghani@hotmail.com>
2022-11-11 20:25:51 +0330

    add metadata and favicons

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,6 +2,9 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         @include('layouts.GA')
+
+        @include('layouts.head')
+
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
---
+++ resources/views/layouts/head.blade.php
@@ -0,0 +1,84 @@
+<link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png">
+<link rel="manifest" href="/favicons/site.webmanifest">
+<link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#5bbad5">
+<meta name="msapplication-TileColor" content="#da532c">
+<meta name="theme-color" content="#ffffff">
+
+<title>L1nc | link shortener</title>
+
+<meta name="description"
+      content="L1nc is a free and open-source url shortener built by Laravel and Livewire"/>
+
+<meta property="og:locale" content="en_US"/>
+<meta property="og:type" content="website"/>
+<meta property="og:title" content="L1nc link shortener"/>
+<meta property="og:description" content="L1nc is a free and open-source link shortener"/>
+<meta property="og:url" content="https://l1nc.com/"/>
+<meta property="og:site_name" content="L1nc link shortener"/>
+
+<script type="application/ld+json" class="yoast-schema-graph">
+        {
+            "@context": "https://schema.org",
+            "@graph": [{
+                "@type": "WebPage",
+                "@id": "https://l1nc.com/",
+                "url": "https://l1nc.com/",
+                "name": "L1nc - link shortener",
+                "isPartOf": {
+                    "@id": "https://l1nc.com/"
+                },
+                "primaryImageOfPage": {
+                    "@id": "https://l1nc.com/images/l1nc-logo.png"
+                },
+                "image": {
+                    "@id": "https://l1nc.com/images/l1nc-logo.png"
+                },
+                "thumbnailUrl": "https://l1nc.com/images/l1nc-logo.png",
+                "datePublished": "2021-11-11T14:00:11+00:00",
+                "dateModified": "2022-11-11T09:07:42+00:00",
+                "author": {
+                    "@id": "https://l1nc.com"
+                },
+                "description": "L1nc is a free and open source link shortener",
+                "inLanguage": "en-US",
+                "potentialAction": [{
+                    "@type": "ReadAction",
+                    "target": ["https://l1nc.com"]
+                }]
+            }, {
+                "@type": "ImageObject",
+                "inLanguage": "en-US",
+                "@id": "https://l1nc.com/images/l1nc-logo.png",
+                "url": "https://l1nc.com/images/l1nc-logo.png",
+                "contentUrl": "https://l1nc.com/images/l1nc-logo.png",
+                "width": 1020,
+                "height": 338
+            }, {
+                "@type": "WebSite",
+                "@id": "https://l1nc.com",
+                "url": "https://l1nc.com",
+                "name": "L1nc",
+                "description": "L1nc is a free and open source link shortener",
+                "inLanguage": "en-US"
+            }
+        ]}
+</script>
+
+
+<meta property="article:publisher" content="https://l1nc.com/">
+<meta property="article:modified_time" content="2022-11-03T20:19:34+00:00">
+
+<meta property="og:image" content="https://l1nc.com/images/l1nc-logo.png">
+<meta property="og:image:width" content="1020">
+<meta property="og:image:height" content="338">
+<meta property="og:image:type" content="image/png">
+
+<meta name="twitter:card" content="summary">
+<meta name="twitter:title" content="URL Shortener - Short URLs & Custom Free Link Shortener | L1nc">
+<meta name="twitter:description"
+      content="L1nc is a free and open-source link shortener which can be self-hosted and installed on your own server">
+<meta name="twitter:image" content="https://l1nc.com/images/l1nc-logo.png">
+<meta name="twitter:site" content="">
+
